12ebc47170d99b7c12dceb4f1089a3557b0657cb
root <root@localhost.localdomain>
2019-09-28 13:55:36 -0400

    Remove, wait_for_it.sh, code merged into django.sh

--- django/wait_for_it.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Waits for postgresql service to be reachable
-
-until psql -h "postgresql" -U "postgres" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping 3"
-  sleep 3
-done
-
->&2 echo "Postgres is up - executing command"
